3157ad210d6181dfa67cf3a6d8e0fbc4c98ecddb
ganesh nikhil <73976037+ganeshnikhil@users.noreply.github.com>
2025-02-15 10:45:26 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import time
 from src.FUNCTION.run_function import execute_function_call
 from src.BRAIN.text_to_info import send_to_ai
-from src.BRAIN.func_call import create_function_call, parse_tool_call
+from src.BRAIN.func_call import create_function_call, parse_tool_calls
 from src.CONVERSATION.speech_to_text import recognize_speech
 from src.CONVERSATION.text_to_speech import speak
 from src.FUNCTION.random_respon import random_choice
 from src.FUNCTION.greet_time import time_of_day
 from src.FUNCTION.Email_send import initate_email 
 from src.DATA.msg import WELCOME_RESPONSES, END_RESPONSES, LISTENING_RESPONSES, NONLISTENING_RESPONSES
+from src.FUNCTION.phone_call import call_phone
+from typing import Union
+
 
 # Function to handle different tasks based on the function name
-def task_action(function_name, response):
+def task_action(function_name:str, response:Union[str , dict , list[str]]) -> None:
     if function_name == "send_to_ai":
         print(f"[= =] AI: \n{response}")
         speak(response)
@@ -29,6 +32,18 @@ def task_action(function_name, response):
                 print("\n\n")
                 time.sleep(0.00001)
     
+    elif function_name == "make_a_call":
+        name = response.get("name")
+        flag = response.get("flag")
+        if flag:
+            speak("Calling "+name)
+            called = call_phone(name)
+            if called:
+                speak("Call is started")
+        else:
+            speak("Failed to start the call")
+            
+
     elif function_name == 'send_email':
         subject = response.get('subject')  # Returns 30
         email_content = response.get('content')
@@ -42,26 +57,29 @@ def task_action(function_name, response):
 
 
 # Function to process commands and call the appropriate functions
-def process_command(command):
+def process_command(command:str) -> None:
     response = create_function_call(command)
-    response_dic = parse_tool_call(response)
-    print(response_dic)
-    if response_dic:
-        func_name = response_dic["name"]
-        prompt = f"Generate a polite loading message for the function '{func_name}' less than 20 words."
-        loading_message = send_to_ai(prompt)
-        speak(loading_message)
-        
-        try:
-            response = execute_function_call(response_dic)
-            if response:
-                task_action(func_name, response)
-        except Exception as e:
-            speak("An unexpected error occurred. Please try again.")
-            print(f"Error: {e}")
+    response_list_dic = parse_tool_calls(response)
+    print(f"Function Map \n {response_list_dic}")
+
+    for response_dic in response_list_dic:
+        print(f"Calling : {response_dic}")
+        if response_dic:
+            func_name = response_dic["name"]
+            prompt = f"Generate a polite loading message for the function '{func_name}' less than 20 words."
+            loading_message = send_to_ai(prompt)
+            speak(loading_message)
+            
+            try:
+                response = execute_function_call(response_dic)
+                if response:
+                    task_action(func_name, response)
+            except Exception as e:
+                speak("An unexpected error occurred. Please try again.")
+                print(f"Error: {e}")
 
 # Function to handle retrying AI calls in case of failure
-def send_to_ai_with_retry(prompt, retries=3, delay=2):
+def send_to_ai_with_retry(prompt:str, retries=3, delay=2) -> None:
     for _ in range(retries):
         try:
             return send_to_ai(prompt)
@@ -79,7 +97,7 @@ def main():
     speak(random_choice(WELCOME_RESPONSES))  # Welcome response
     last_speech_time = time.time()  # Track the last time speech was recognized
     timeout_duration = 300  # Timeout after 5 minutes of inactivity
-
+    stop_duration = 60
     while True:
         spoken_text = recognize_speech()
 
@@ -89,6 +107,8 @@ def main():
         
         spoken_text = spoken_text.lower().strip()
         print(f"You said: {spoken_text}")
+        
+            
         # If user says 'exit', terminate the program
         if spoken_text == "exit":
             print("[= =] Exiting...")
@@ -106,19 +126,21 @@ def main():
 
         # If in listening mode, handle user commands
         if listening_mode:
-            if "stop" in spoken_text:
+            if "stop" not in spoken_text:
+                full_text += spoken_text + " "  # Append spoken text to full_text for command processing
+                print(f"[*] Current text: {full_text}")
+                
+            if "stop" in spoken_text or ((time.time() - last_speech_time) > stop_duration):
                 process_command(full_text)
                 listening_mode = False  # Deactivate listening mode
                 full_text = ""  # Reset full_text to empty
                 print("[+] Listening mode deactivated.")
                 speak(random_choice(NONLISTENING_RESPONSES))  # Response when stopping listening
                 time.sleep(0.2)
-            else:
-                full_text += spoken_text + " "  # Append spoken text to full_text for command processing
-                print(f"[*] Current text: {full_text}")
-
+                
+                
         # If the user has been inactive for too long, deactivate listening mode
-        if time.time() - last_speech_time > timeout_duration and listening_mode:
+        if (time.time() - last_speech_time > timeout_duration) and listening_mode:
             speak("Listening mode has timed out due to inactivity.")
             listening_mode = False
             full_text = ""  # Reset full_text
